75e54e637a5e8b4c7bd9aa18e9a3d0abd629f446
Alexander Kmoch <allixender@googlemail.com>
2017-01-10 14:52:41 +1300

    unbelievable typo

--- build-script.sh
+++ build-script.sh
@@ -89,7 +89,7 @@ wget -nv -O $BUILD_ROOT/pest13-trusty64-master.zip https://github.com/allixender
   && make -f pestutl1.mak all && make -f pestutl2.mak all && make -f pestutl3.mak all \
   && make -f pestutl4.mak all && make -f pestutl5.mak all && make -f pestutl6.mak all \
   && make -f sensan.mak all && make -f beopest.mak all \
-  && for tool in $PEST_TOOLS do; cp $tool $CGI_DIR; done || exit 1
+  && for tool in $PEST_TOOLS; do cp $tool $CGI_DIR; done || exit 1
 
 # however, auto additonal packages won't get removed
 # maybe auto remove is a bit too hard
